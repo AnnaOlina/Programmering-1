9decf5ec4d82a5025200821628130bc7e33d6a9a
Anna Lundin <aolu71339@edu.ucl.dk>
2025-04-02 08:13:11 +0200

    Ændret i Main, hovedmenu. Indsat while-løkke.

--- Program.cs
+++ Program.cs
@@ -9,50 +9,56 @@ namespace Programmering_1
          */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hovedmenu\nTast relevant tal og tryk enter for at valg af nedenstående: \n");
-            Console.WriteLine("1. Read Car Details.\n");
-            Console.WriteLine("2. Drive.\n");
-            Console.WriteLine("3. IsPalindrome.\n");
-            Console.WriteLine("4. Print Car Details.\n");
-            Console.WriteLine("5. Afslut programmet.\n");
-
-            int værdi = int.Parse(Console.ReadLine());
-            Menu(værdi);
-
-            Console.ReadKey();
-        }
-
-        static void Menu(int værdi)
-        {
-            switch (værdi)
+            while (true)
             {
-                case 1:
-                    Console.WriteLine("\nReadCarDetails.");
-                    ReadCarDetailsMenuItem.StartReadCarDetailsMenu();
-                    break;
+                Console.WriteLine("Hovedmenu\nTast relevant tal og tryk enter for at valg af nedenstående: \n");
+                Console.WriteLine("1. Read Car Details.\n");
+                Console.WriteLine("2. Drive.\n");
+                Console.WriteLine("3. IsPalindrome.\n");
+                Console.WriteLine("4. Print Car Details.\n");
+                Console.WriteLine("5. Afslut programmet.\n");
 
-                case 2:
-                    Console.WriteLine("\nDrive");
-                    DriveMenuItem.StartDriveMenu();
-                    break;
+                int værdi = int.Parse(Console.ReadLine());
+                Menu(værdi);
 
-                case 3:
-                    Console.WriteLine("\nIsPalindrome.");
-                    IsPalindromeMenuItem.StartIsPalindromeMenu();
-                    break;
+                Console.ReadKey();
+            }
 
-                case 4:
-                    Console.WriteLine("\nPrint Car Details.");
-                    PrintCarDetailsMenuItem.StartPrintCarDetailsMenu();
-                    break;
+            static void Menu(int værdi)
+            {
+                switch (værdi)
+                {
+                    case 1:
+                        Console.WriteLine("\nReadCarDetails.");
+                        ReadCarDetailsMenuItem.StartReadCarDetailsMenu();
+                        break;
 
-                case 5:
-                    Console.WriteLine("\nAfslut programmet.");
-                    break;
+                    case 2:
+                        Console.WriteLine("\nDrive");
+                        DriveMenuItem.StartDriveMenu();
+                        break;
 
-                default:
-                    Console.WriteLine(værdi + " er ikke en mulighed. Vælg et tal mellem 1 og 5.");
-                    break;
+                    case 3:
+                        Console.WriteLine("\nIsPalindrome.");
+                        IsPalindromeMenuItem.StartIsPalindromeMenu();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\nPrint Car Details.");
+                        PrintCarDetailsMenuItem.StartPrintCarDetailsMenu();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("\nAfslut programmet.");
+                        break;
+
+                    default:
+                        Console.WriteLine(værdi + " er ikke en mulighed. Vælg et tal mellem 1 og 5.");
+                        break;
+                }
+
+                Console.WriteLine("Tryk på en tast for at fortsætte...");
+                Console.ReadKey();
             }
         }
     }
